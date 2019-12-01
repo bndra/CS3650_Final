b9d607d5b85ad9b5c569aeab29487f8d74c70a7b
Kevin Tang <ktang20@gmail.com>
2019-11-30 21:29:40 -0800

    Update DeutschOracle.py

--- DeutschOracle.py
+++ DeutschOracle.py
@@ -12,13 +12,15 @@ from qiskit.compiler import transpile, assemble
 from qiskit.tools.jupyter import *
 # Loading your IBM Q account(s)
 IBMQ.load_accounts()
+provider = IBMQ.get_provider(hub='ibm-q')
+provider.backends()
 
 
 # In[2]:
 
 
 import qiskit as qk
-#IBMQ.save_account('auth key goes here')
+#IBMQ.save_account('not publicly displaying my auth key')
 
 
 # In[3]:
@@ -99,106 +101,102 @@ testConst1_1 = flipInput0 + Const1 + Zmeasure #"    " 1
 #we recieve outputs[2]
 #where outputs[0] = inputs[0]
 #and   outputs[1] = f(outputs[0])
-#which means whatever numbers you see, apply the operator to the lsb to obtain the msb
-
 
 # In[10]:
 
-
-job = qk.execute([testID0,testID1,testNot0,testNot1,testConst0_0,testConst0_1,testConst1_0,testConst1_1],backend=IBMQ.get_backend('ibmq_qasm_simulator',hub=None),shots=1)
+#using a quantum simulator provided by IBM
+job = qk.execute([testID0,testID1,testNot0,testNot1,testConst0_0,testConst0_1,testConst1_0,testConst1_1],backend=provider.get_backend('ibmq_qasm_simulator'),shots=1)
 
 
 # In[11]:
 
 
-job.result().get_counts(testID0)
-
-
-# In[12]:
-
-
-job.result().get_counts(testID1)
-
+job.result().get_counts(testID0) #output is of this format: {'outputbit inputbit': numberofoccurences}
+#which means we should see {'00':1}
+job.result().get_counts(testID1) 
+#should see {'11':1}
+#the output bit changes based on the input bit, we can see in 2 queries that this is variable
 
 # In[13]:
 
 
-job.result().get_counts(testNot0)
-
-
-# In[14]:
-
-
-job.result().get_counts(testNot1)
-
+job.result().get_counts(testNot0) 
+# {'10':1}
+job.result().get_counts(testNot1) 
+#{'01':1}
+#output depends on input, 2 queries to verify variable
 
 # In[15]:
 
 
-job.result().get_counts(testConst0_0)
-
-
-# In[16]:
-
-
-job.result().get_counts(testConst0_1)
-
+job.result().get_counts(testConst0_0) 
+#{'00':1}
+job.result().get_counts(testConst0_1) 
+#{'01':1}
+#output stays 0 regardless of input, function is constant
 
 # In[17]:
 
 
 job.result().get_counts(testConst1_0)
-
-
-# In[18]:
-
-
+#{'10':1}
 job.result().get_counts(testConst1_1)
-
+#{'11':1}
+#output is 1 regardless of input, constant
 
 # In[19]:
 
 
 #Superposition tests. Can we tell the difference between const and variable operators in a single query??
-testID = DeutschOracle + ID + Xmeasure
-testNot = DeutschOracle + Not + Xmeasure
+testID = DeutschOracle + ID + Xmeasure          #difference between these and the classical bits:
+testNot = DeutschOracle + Not + Xmeasure        #set bits to 1 and hadamard before and hadamard after the function then measure.
 testConst0 = DeutschOracle + Const0 + Xmeasure
 testConst1 = DeutschOracle + Const1 + Xmeasure
-#outputs[0] = 0 indicates a variable operator
-#outputs[0] = 1 indicates a const operator
-
+#output format is slightly different for these. 
+#the second bit is the important one to look at
+#if it's 0, the function is variable, if 1 the function is constant
+#first bit should always be 1, but sometimes there are errors reading the qbits
+#10 for variable functions: ID, NOT 
+#11 for const functions : Const0, Const1
 
 # In[20]:
 
-
-quantumJob = qk.execute([testID,testNot,testConst0,testConst1],backend=IBMQ.get_backend('ibmqx2',hub=None),shots=300)
-
+#using one of IBM's actual quantum computers: ibmqx2
+quantumJob = qk.execute([testID,testNot,testConst0,testConst1],backend=provider.get_backend('ibmqx2'),shots=300) 
 
 # In[21]:
 
 
-quantumJob.result().get_counts(testID)
+quantumJob.result().get_counts(testID)#ID is a variable function so we should see a second bit of 0
+#{'00': 8, '01': 22, '11': 80, '10': 190} getting 10 the most often by far, consistent
 
 
 # In[22]:
 
 
-quantumJob.result().get_counts(testNot)
-
+quantumJob.result().get_counts(testNot)#Not is also variable, should see 0 as well
+#{'00': 12, '01': 24, '11': 82, '10': 182} getting 10 most often again, consistent
 
 # In[23]:
 
 
 quantumJob.result().get_counts(testConst0)
-
+#{'01': 14, '11': 282, '10': 4} getting 11 most often, good
 
 # In[24]:
 
 
 quantumJob.result().get_counts(testConst1)
+#{'01': 10, '11': 283, '10': 7} getting 11 most often, good
 
 
-# In[ ]:
+
+# imagine if these four unary operators were black boxes and we wanted to identify which were constant/variable
+# that's essentially the Deutsch Oracle question, and we've shown here that it takes 2 queries to answer it 
+# using classical bits, and only a single query with qbits.
+# why this happens is a bit difficult to answer, but I can say for certain that our unary operators here
+# have drastically different effects on bits in superposition than they do on eigenvectors, at that it's 
+# possible to use this to infer relationships between the bits rather than their exact values.
 
 
 
